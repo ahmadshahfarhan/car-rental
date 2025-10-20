52f99b9d0455f3c7b65b647fad59158ce768a842
ahmadshahfarhan <98960641+ahmadshahfarhan@users.noreply.github.com>
2025-10-21 02:31:44 +0700

    day 1

--- src/components/home/hero/components/formBanner.jsx
+++ src/components/home/hero/components/formBanner.jsx
@@ -2,8 +2,16 @@ const formBanner = () => {
   return (
     <>
       <section>
-        <div className="">
-
+        <div className=" bg-surface rounded-[20px] w-[416px]">
+          <form className=" p-[40px] flex flex-col justify-center items-center">
+            <label
+              htmlFor=""
+              className=" font-worksans font-semibold text-[28px] text-text-main"
+            >
+              Book your car
+            </label>
+             <button className="btn-orange w-full">Book now</button>
+          </form>
         </div>
       </section>
     </>
